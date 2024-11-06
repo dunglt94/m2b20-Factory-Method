bb5d18a519de79c4c29418de2d35f151739427c6
dunglt94 <tuandung9594@gmail.com>
2024-11-06 14:06:24 +0700

    first add

---
+++ src/model/Animal.java
@@ -0,0 +1,5 @@
+package model;
+
+public abstract class Animal {
+    public abstract String makeSound();
+}
---
+++ src/model/Cat.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Cat extends Animal {
+    @Override
+    public String makeSound() {
+        return "meow";
+    }
+}
